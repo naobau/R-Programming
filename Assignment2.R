5977b3a836780fc0a5eb539ac72e7d9004aab51b
naobau <111362768+naobau@users.noreply.github.com>
2024-09-08 21:54:22 -0400

    Assignment2.R

--- Assignment2.R
+++ Assignment2.R
@@ -1,11 +1,3 @@
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
 myMean <- function(assignment2) { return(sum(assignment)/length(someData))
 }
-assignment2
-[1] 16 18 14 22 27 17 19 17 17 22 20 22
-> myMean(assignment2)
-Error in myMean(assignment2) : object 'someData' not found
-> assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
-> myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
-> myMean(assignment2)
-[1] 19.25
